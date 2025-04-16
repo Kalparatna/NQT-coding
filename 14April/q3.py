599f9b142ada1c30c6bd03488ab14369258ba4e8
Kalparatna <kalparatna223@gmail.com>
2025-04-16 16:38:46 +0530

    14April/q3.py

--- 14April/q3.py
+++ 14April/q3.py
@@ -35,3 +35,26 @@ Explanation: We need the sum of 1st, 2nd, 3rd, and 4th prime numbers.
 Sum = 2 + 3 +5 + 7 = 17
 
 '''
+def is_prime(n):
+    if n ==0 or n==1:
+        return False
+    
+    for i in range(2, int(n ** 0.5) + 1): 
+        if n % i == 0:
+            return False
+        
+    return True
+
+m,n = map(int, input().split())
+sum_prime = 0
+count = 0
+i = 2
+
+while count < m+n:
+    if is_prime(i):
+        count += 1
+        if count >= m:
+            sum_prime += i
+    i += 1
+
+print(sum_prime)
